dc4195d40090ce37626e2f41de0bf9cb30a967be
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-27 20:23:53 +0530

    stable animation

--- app/components/CustomCursor.tsx
+++ app/components/CustomCursor.tsx
@@ -3,17 +3,17 @@
 import { useCursor } from "../hooks/useCursor";
 
 export const CustomCursor = () => {
-	return (
-		<>
-			{Array.from({ length: 10 }).map((_, i) => (
-				<div
-					key={i}
-					className="ball  mix-blend-difference  fixed  "
-					style={{
-						backgroundColor: `hsl(10,10%,${100 - i * 2}%)`,
-					}}
-				></div>
-			))}
-		</>
-	);
+  return (
+    <>
+      {Array.from({ length: 10 }).map((_, i) => (
+        <div
+          key={i}
+          className="ball rounded-full  mix-blend-difference  fixed  "
+          style={{
+            backgroundColor: `hsl(10,10%,${100 - i * 2}%)`,
+          }}
+        ></div>
+      ))}
+    </>
+  );
 };
--- app/hooks/useCursor.tsx
+++ app/hooks/useCursor.tsx
@@ -2,31 +2,32 @@ import gsap from "gsap";
 import { useEffect, useState } from "react";
 
 export const useCursor = () => {
-	const [isMouseEnter, setIsMouseEnter] = useState(false);
-	const [id, setId] = useState<string>("");
-	useEffect(() => {
-		let targets = gsap.utils.toArray(".ball");
+  const [isMouseEnter, setIsMouseEnter] = useState(false);
+  const [id, setId] = useState<string>("");
+  useEffect(() => {
+    let targets = gsap.utils.toArray(".ball");
 
-		const hoverElement = document.getElementById(id);
-		const rect = hoverElement?.getBoundingClientRect();
-		const animation = (e: MouseEvent) => {
-			console.log(e.clientX, hoverElement?.offsetTop);
-			gsap.to(targets, {
-				duration: 0.5,
-				x: hoverElement ? rect?.left : e.clientX,
-				y: hoverElement ? rect?.top : e.clientY,
-				height: hoverElement?.clientHeight ? hoverElement.clientHeight : 10,
-				width: hoverElement?.clientWidth ? hoverElement.clientWidth : 10,
-				ease: "power1.out",
-				overwrite: "auto",
-				stagger: 0.1,
-				backgroundColor: isMouseEnter ? "red" : "white",
-			});
-		};
-		window.addEventListener("mousemove", animation);
+    const hoverElement = document.getElementById(id);
+    const rect = hoverElement?.getBoundingClientRect();
+    const animation = (e: MouseEvent) => {
+      console.log(e.clientX, hoverElement?.offsetTop);
+      gsap.to(targets, {
+        duration: 0.5,
+        x: hoverElement ? rect?.left : e.clientX,
+        y: hoverElement ? rect?.top : e.clientY,
+        height: hoverElement?.clientHeight ? hoverElement.clientHeight : 10,
+        width: hoverElement?.clientWidth ? hoverElement.clientWidth : 10,
+        ease: "power1.out",
+        overwrite: "auto",
+        stagger: 0.1,
+        backgroundColor: isMouseEnter ? "red" : "white",
+        borderRadius: isMouseEnter ? "0%" : "50%",
+      });
+    };
+    window.addEventListener("mousemove", animation);
 
-		return () => window.removeEventListener("mousemove", animation);
-	}, [isMouseEnter, id]);
+    return () => window.removeEventListener("mousemove", animation);
+  }, [isMouseEnter, id]);
 
-	return { isMouseEnter, setIsMouseEnter, setId };
+  return { isMouseEnter, setIsMouseEnter, setId };
 };
