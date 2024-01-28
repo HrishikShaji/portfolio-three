3813041eb946de46694b9a33e5b757e31d7b6b87
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-28 23:50:51 +0530

    added mouse over animation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,52 +2,32 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import Link from "next/link";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  useCallback,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import { CursorContext } from "../providers/CursorContext";
 
 export const Navbar = () => {
-  const overlay = useRef();
-
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  useLayoutEffect(() => {
-    if (isMounted) {
-      let ctx = gsap.context(() => {
-        gsap.set(overlay.current, { scaleX: 0 });
-      });
-
-      return () => ctx.revert();
-    }
-  }, [isMounted]);
-
-  const onMouseEnter = ({ currentTarget }) => {
-    let q = gsap.utils.selector(currentTarget);
-
-    gsap.to(q(".overlay"), { scaleX: 1, transformOrigin: "left" });
-  };
-
-  const onMouseLeave = ({ currentTarget }) => {
-    let q = gsap.utils.selector(currentTarget);
-
-    gsap.to(q(".overlay"), { scaleX: 0, transformOrigin: "right" });
-  };
+  const [, setCursor] = useContext(CursorContext);
+  const toggleCursor = useCallback(() => {
+    setCursor(({ active }) => ({ active: !active }));
+  });
   return (
     <div className="w-full bg-transparent p-10 items-center flex  justify-between">
       <h1 className="text-5xl ">ANAKIN</h1>
       <div className={`flex gap-3 items-center text-xl leading-none`}>
         <Link
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
+          onMouseEnter={toggleCursor}
+          onMouseLeave={toggleCursor}
           className="button relative border-[2px] border-white py-1 px-2"
           href="/projects"
         >
-          <div
-            ref={overlay}
-            className="overlay absolute left-0 top-0 w-full h-full bg-white"
-          />
+          <div className="overlay absolute left-0 top-0 w-full h-full bg-white" />
           <h1 className="mix-blend-difference text-white">PROJECTS</h1>
         </Link>
 
