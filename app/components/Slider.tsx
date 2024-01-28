a1cab4d5cbd5f0644925f6da18b703162055a152
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-28 21:48:25 +0530

    styling improved

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -2,140 +2,111 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 interface SliderProps {
-  images: any[];
+	images: any[];
 }
 
-export const Slider = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [direction, setDirection] = useState(null);
+export const Slider: React.FC<SliderProps> = ({ images }) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const [direction, setDirection] = useState<string | null>(null);
 
-  const slideVariants = {
-    hiddenRight: {
-      x: "100%",
-      opacity: 0,
-    },
-    hiddenLeft: {
-      x: "-100%",
-      opacity: 0,
-    },
-    visible: {
-      x: "0",
-      opacity: 1,
-      transition: {
-        duration: 1,
-      },
-    },
-    exit: {
-      opacity: 0,
-      scale: 0.8,
-      transition: {
-        duration: 0.5,
-      },
-    },
-  };
-  const slidersVariants = {
-    hover: {
-      scale: 1.2,
-      backgroundColor: "#ff00008e",
-    },
-  };
-  const dotsVariants = {
-    initial: {
-      y: 0,
-    },
-    animate: {
-      y: -10,
-      scale: 1.2,
-      transition: { type: "spring", stiffness: 1000, damping: "10" },
-    },
-    hover: {
-      scale: 1.1,
-      transition: { duration: 0.2 },
-    },
-  };
+	const slideVariants = {
+		hiddenRight: {
+			x: "100%",
+			opacity: 0,
+		},
+		hiddenLeft: {
+			x: "-100%",
+			opacity: 0,
+		},
+		visible: {
+			x: "0",
+			opacity: 1,
+			transition: {
+				duration: 1,
+			},
+		},
+		exit: {
+			opacity: 0,
+			scale: 0.8,
+			transition: {
+				duration: 0.5,
+			},
+		},
+	};
+	const slidersVariants = {
+		hover: {
+			scale: 1.2,
+			backgroundColor: "#ff00008e",
+		},
+	};
+	const dotsVariants = {
+		initial: {
+			y: 0,
+		},
+		animate: {
+			y: -10,
+			scale: 1.2,
+			transition: { type: "spring", stiffness: 1000, damping: "10" },
+		},
+		hover: {
+			scale: 1.1,
+			transition: { duration: 0.2 },
+		},
+	};
 
-  const handleNext = () => {
-    setDirection("right");
-    setCurrentIndex((prevIndex) =>
-      prevIndex + 1 === images.length ? 0 : prevIndex + 1,
-    );
-  };
+	const handleNext = () => {
+		setDirection("right");
+		setCurrentIndex((prevIndex) =>
+			prevIndex + 1 === images.length ? 0 : prevIndex + 1,
+		);
+	};
 
-  const handlePrevious = () => {
-    setDirection("left");
+	const handlePrevious = () => {
+		setDirection("left");
 
-    setCurrentIndex((prevIndex) =>
-      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1,
-    );
-  };
+		setCurrentIndex((prevIndex) =>
+			prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1,
+		);
+	};
 
-  const handleDotClick = (index) => {
-    setDirection(index > currentIndex ? "right" : "left");
-    setCurrentIndex(index);
-  };
+	const handleDotClick = (index: number) => {
+		setDirection(index > currentIndex ? "right" : "left");
+		setCurrentIndex(index);
+	};
 
-  return (
-    <div className="carousel">
-      <div className="relative rounded-[10px] h-[400px] max-w-[650px] m-auto overflow-hidden">
-        <AnimatePresence>
-          <motion.img
-            className="w-[99%] h-[99%] rounded-[8px] border-white"
-            key={currentIndex}
-            src={images[currentIndex].img}
-            initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
-            animate="visible"
-            exit="exit"
-            variants={slideVariants}
-          />
-        </AnimatePresence>
-        <div className=" flex justify-between">
-          <motion.div
-            variants={slidersVariants}
-            whileHover="hover"
-            className="left-0 absolute bg-neutral-500 p-3 rounded-full top-0 h-[25px] w-[25px]"
-            onClick={handlePrevious}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="20"
-              viewBox="0 96 960 960"
-              width="20"
-            >
-              <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
-            </svg>
-          </motion.div>
-          <motion.div
-            variants={slidersVariants}
-            whileHover="hover"
-            className="right-0 absolute bg-neutral-500 p-3 rounded-full top-0 h-[25px] w-[25px]"
-            onClick={handleNext}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="20"
-              viewBox="0 96 960 960"
-              width="20"
-            >
-              <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
-            </svg>
-          </motion.div>
-        </div>
-      </div>
-      <div className="carousel-indicator mt-[20px] flex justify-center gap-[20px]">
-        {images.map((_, index) => (
-          <motion.div
-            key={index}
-            className={`bg-red-500 w-[15px] h-[15px] rounded-full ${
-              currentIndex === index ? "bg-blue-500" : ""
-            }`}
-            onClick={() => handleDotClick(index)}
-            initial="initial"
-            animate={currentIndex === index ? "animate" : ""}
-            whileHover="hover"
-            variants={dotsVariants}
-          ></motion.div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="carousel flex flex-col gap-5">
+			<div className="relative bg-green-500  h-[40vh] w-full  overflow-hidden">
+				<AnimatePresence>
+					<motion.img
+						className="w-full h-full object-cover"
+						key={currentIndex}
+						src={images[currentIndex].img}
+						initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
+						animate="visible"
+						exit="exit"
+						variants={slideVariants}
+					/>
+				</AnimatePresence>
+			</div>
+			<div className=" flex text-2xl justify-between items-center text-red-500 ">
+				<motion.button onClick={handlePrevious}>PREV</motion.button>
+				<div className="carousel-indicator  flex justify-center gap-[20px]">
+					{images.map((_, index) => (
+						<motion.div
+							key={index}
+							className={` cursor-pointer w-[30px] h-[5px]  ${currentIndex === index ? "bg-red-500" : "bg-neutral-500"
+								}`}
+							onClick={() => handleDotClick(index)}
+							initial="initial"
+							animate={currentIndex === index ? "animate" : ""}
+							whileHover="hover"
+							variants={dotsVariants}
+						></motion.div>
+					))}
+				</div>
+				<motion.button onClick={handleNext}>NEXT</motion.button>
+			</div>
+		</div>
+	);
 };
