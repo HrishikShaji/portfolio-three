a988c8b7e07921c861733c7bba48af41a476feb3
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-28 18:54:03 +0530

    slider added

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,10 +1,11 @@
-import { Carousel } from "./Carousel";
+import { data } from "../lib/data";
+import { Slider } from "./Slider";
 
 export const Projects = () => {
-	return (
-		<div className="h-full w-full p-10  flex flex-col gap-10">
-			<h1 className="text-3xl text-red-500">PROJECTS</h1>
-			<Carousel />
-		</div>
-	);
+  return (
+    <div className="h-full w-full p-10  flex flex-col gap-10">
+      <h1 className="text-3xl text-red-500">PROJECTS</h1>
+      <Slider images={data.projects.data} />
+    </div>
+  );
 };
