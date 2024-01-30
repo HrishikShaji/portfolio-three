1c847bda88fd5d27b1bc38c2993f4b8351069389
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-30 09:50:13 +0530

    animated each skill

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,26 +1,20 @@
 import { useCursor } from "../hooks/useCursor";
 import { data } from "../lib/data";
+import { motion } from "framer-motion";
 
 export const Skills = () => {
-	const { setId, setIsMouseEnter } = useCursor();
 	return (
 		<div className="h-full w-full   flex flex-col p-2 ">
 			<div className="grid grid-cols-4 gap-4 text-3xl">
 				{data.skills.data.map((item, i) => (
-					<div
-						id={`${item}-button`}
-						onMouseEnter={() => {
-							setIsMouseEnter(true);
-							setId(`${item}-button`);
-						}}
-						onMouseLeave={() => {
-							setIsMouseEnter(false);
-							setId("");
-						}}
-						key={i}
-						className="hover-text bg-red-500 flex flex-col gap-1 p-2 text-black  "
-					>
-						<h1>{item}</h1>
+					<div key={i} className="flex gap-2 h-[50px] relative   items-center">
+						<motion.div
+							initial={{ scaleX: 0.1, originX: "left" }}
+							whileHover={{ scaleX: 1 }}
+							transition={{ duration: 1 }}
+							className="w-full h-full  bg-red-500 absolute mix-blend-difference"
+						/>
+						<h1 className="text-2xl pl-[25px] leading-tight">{item}</h1>
 					</div>
 				))}
 			</div>
